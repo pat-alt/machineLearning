b52021ce64d2a75cd54856c89827a6f63bec9ec4
pat-alt <altmeyerpat@gmail.com>
2021-02-08 16:21:41 +0100

    computations done

---
+++ R/knn_regressor.R
@@ -0,0 +1,13 @@
+nn_reg <- function(X,y,k, ...) {
+  row_idx <- 1:nrow(X)
+  distances <- data.table(t(combn(row_idx,2)))
+  distances[,dist:=c(dist(X,...))]
+  distances_rev <- copy(distances)
+  setnames(distances_rev, c("V1", "V2"), c("V2", "V1"))
+  distances <- rbind(distances, distances_rev)
+  setorder(distances, V1, dist)
+  setnames(distances, c("V1", "V2"), c("X", "neighbour"))
+  distances[,y_neighbour:=y[neighbour]]
+  fitted <- distances[,mean(y_neighbour[1:k]),by=X]$V1
+  return(fitted)
+}
